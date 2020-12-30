47bd1ada30f582f24acdcddb1f0b143ef47e56f2
paulr <paul.riedlingen@gmail.com>
2020-12-30 13:00:49 +0100

    basic clock is functional

---
+++ android/app/src/main/kotlin/de/hfugurken/wecker/MainActivity.kt
@@ -0,0 +1,6 @@
+package de.hfugurken.wecker
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/global_data.dart
@@ -0,0 +1,5 @@
+library global_data;
+import 'dart:ui';
+
+
+const BACKGROUND_COLOR = const Color.fromRGBO(4, 32, 51, 1);
