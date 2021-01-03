e2e419d09c60ce4cf983d38068e51386fe7a0bc6
paulr <paul.riedlingen@gmail.com>
2021-01-03 14:16:42 +0100

    adopted settings page

--- lib/global_data.dart
+++ lib/global_data.dart
@@ -1,7 +1,6 @@
 library global_data;
 import 'dart:ui';
 
-
 const Color BACKGROUND_COLOR = const Color.fromRGBO(4, 32, 51, 1);
 /*
 const Map ex = {
--- lib/settings_page.dart
+++ lib/settings_page.dart
@@ -11,102 +11,114 @@ class SettingsPage extends StatelessWidget {
       child: Scaffold(
           backgroundColor: BACKGROUND_COLOR,
           body: Center(
-            child: Column(
+            child: Container(
+              padding: EdgeInsets.all(16),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Container(height: 32,),
 
-              crossAxisAlignment: CrossAxisAlignment.start,
+                  Text("Einstellungen",
 
-              children: [
-                Container(height: 32,),
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontFamily: 'Roboto',
+                      fontSize: 30,
+                    ),
+                  ),
+                  RaisedButton(
+                    child: Text("Back"),
+                    onPressed: (){
+                      Navigator.pop(context);
+                    },
+                  ),
+                  Text("Uhr",
+                    style: TextStyle(
+                      color: Colors.blue,
+                      fontFamily: 'Roboto',
+                      fontSize: 20,
+                    ),
+                  ),
+                  Container(height: 15,),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text("Zeit mit Sekunden anzeigen",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontFamily: 'Roboto',
+                          fontSize: 20,
+                        ),
+                      ),
+                      Switch(
+                        value: true,
+                        onChanged: (bool active){
 
-                Text("Einstellungen",
+                        },
+                      ),
+                    ],
+                  ),
+                  Container(height: 15,),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text("Datum und Uhrzeit ändern",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontFamily: 'Roboto',
+                          fontSize: 20,
+                        ),
+                      ),
+                      Switch(
+                        value: true,
+                        onChanged: (bool active){
 
-                  style: TextStyle(
+                        },
+                      ),
+                    ],
+                  ),
+                  Container(height: 15,),
+                  Divider(
                     color: Colors.white,
-                    fontFamily: 'Roboto',
-                    fontSize: 30,
                   ),
-                ),
-                RaisedButton(
-                  child: Text("Back"),
-                  onPressed: (){
-                    Navigator.pop(context);
-                  },
-                ),
-                Text("Uhr",
-                  style: TextStyle(
-                    color: Colors.blue,
-                    fontFamily: 'Roboto',
-                    fontSize: 20,
-                  ),
-                ),
-                Container(height: 15,),
-                Text("Zeit mit Sekunden anzeigen",
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontFamily: 'Roboto',
-                    fontSize: 20,
-                  ),
-                ),
-                Switch(
-                  value: true,
-                  onChanged: (bool active){
+                  Container(height: 15,),
 
-                  },
-                ),
-                Container(height: 15,),
-                Text("Datum und Uhrzeit ändern",
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontFamily: 'Roboto',
-                    fontSize: 20,
+                  Text("Wecker",
+                    style: TextStyle(
+                      color: Colors.blue,
+                      fontFamily: 'Roboto',
+                      fontSize: 20,
+                    ),
                   ),
-                ),
-                Switch(
-                  value: true,
-                  onChanged: (bool active){
+                  Container(height: 15,),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text("Einschlafzeit mittels FitBit ermitteln",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontFamily: 'Roboto',
+                          fontSize: 20,
+                        ),
+                      ),
+                      Switch(
+                        value: true,
+                        onChanged: (bool active){
 
-                  },
-                ),
-                Container(height: 15,),
-                Text("_____________________________________________",
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontFamily: 'Roboto',
-                    fontSize: 20,
+                        },
+                      ),
+                    ],
                   ),
-                ),
-                Container(height: 15,),
-
-                Text("Wecker",
-                  style: TextStyle(
-                    color: Colors.blue,
-                    fontFamily: 'Roboto',
-                    fontSize: 20,
+                  Container(height: 15,),
+                  Text("Zeit zum Einschlafen",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontFamily: 'Roboto',
+                      fontSize: 20,
+                    ),
                   ),
-                ),
-                Container(height: 15,),
-                Text("Einschlafzeit mittels FitBit ermitteln",
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontFamily: 'Roboto',
-                    fontSize: 20,
-                  ),
-                ),
-                Switch(
-                  value: true,
-                  onChanged: (bool active){
-
-                  },
-                ),
-                Container(height: 15,),
-                Text("Zeit zum Einschlafen",
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontFamily: 'Roboto',
-                    fontSize: 20,
-                  ),
-                ),
-              ],
+                ],
+              ),
             )
           )
       ),
